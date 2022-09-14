09c3353cca9aa985a1aaeee37f456e551fa84b79
rifat32 <rifatbilalphilips@gmail.com>
2022-09-14 19:56:51 +0600

    update

--- app/Http/Services/PatientService.php
+++ app/Http/Services/PatientService.php
@@ -76,7 +76,7 @@ trait PatientService
     }
     public function getMainPatientsService($request)
     {
-        $patients =   Patient::with("sales.saleDetails","sales.payments")
+        $patients =   Patient::with("sales.saleDetails","sales.payments","prescriptions.payments")
         ->leftJoin('prescribtions', 'patients.id', '=', 'prescribtions.patient_id')
         ->leftJoin('sales', 'patients.id', '=', 'sales.patient_id')
         ->where(function ($query) {
@@ -84,60 +84,7 @@ trait PatientService
 
             ->where("sales.id","!=",NULL)
             ->orWhere("prescribtions.id","!=",NULL);
-        })
-
-
-
-        ->select(
-            "patients.id",
-            "patients.name",
-            "patients.phone",
-            "patients.address",
-            "patients.sex",
-            "patients.birth_date",
-            DB::raw("SUM(IF(sales.status = 'Confirmed', (SELECT SUM(paid_amount) FROM sale_payments WHERE sale_payments.sale_id=sales.id), 0)) as paid"),
-            DB::raw("SUM(IF(sales.status = 'Confirmed', (SELECT SUM(line_discount) FROM sale_details WHERE sale_details.sale_id=sales.id), 0)) as line_discount"),
-            DB::raw("SUM(IF(sales.status = 'Confirmed', (SELECT SUM(amount) FROM sale_details WHERE sale_details.sale_id=sales.id), 0)) as sub_total"),
-            DB::raw("SUM(IF(sales.status = 'Confirmed', sales.discount, 0)) as discount"),
-
-            DB::raw("(sales.discount + SUM(IF(sales.status = 'Confirmed', (SELECT SUM(line_discount) FROM sale_details WHERE sale_details.sale_id=sales.id), 0))) as total_discount"),
-            // DB::raw("
-            // (
-            //     (SUM(IF(sales.status = 'Confirmed', (SELECT SUM(amount) FROM sale_details WHERE sale_details.sale_id=sales.id), 0))
-            //     -
-            //     (
-            //         SUM(sales.discount)
-            //     +
-            //      SUM(IF(sales.status = 'Confirmed', (SELECT SUM(line_discount) FROM sale_details WHERE sale_details.sale_id=sales.id), 0))
-
-            //      ))
-
-            //      -
-
-            //      SUM(IF(sales.status = 'Confirmed', (SELECT SUM(paid_amount) FROM sale_payments WHERE sale_payments.sale_id=sales.id), 0))
-
-            // ) as total_sale_due
-
-            // "),
-            DB::raw("
-                SUM(prescribtions.fees) as total_prescription_fees
-            "),
-            DB::raw(" SUM( (SELECT SUM(amount) FROM prescription_payments WHERE prescription_payments.prescription_id=prescribtions.id))
-
-            as total_prescription_paid
-
-            "),
-            DB::raw("
-            (
-                SUM(prescribtions.fees) -
-
-            SUM( (SELECT SUM(amount) FROM prescription_payments WHERE prescription_payments.prescription_id=prescribtions.id))
-            )
-            as total_prescription_due
-
-            "),
-
-        );
+        });
 
         if($request->has("search")) {
             $patients =  $patients->where(function($q){
